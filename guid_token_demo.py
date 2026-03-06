4f992d2feeb4da7961672408f8cdc320a95f62bd
Frederic Malo <frederic.malo@checkmarx.com>
2026-03-06 11:52:05 +0100

    guid

--- guid_token_demo.py
+++ guid_token_demo.py
@@ -1,1 +1,5 @@
-my-guid-token = "guid_6B29FC40-CA47-1067-B31D-00DD010662DA"
+
+
+my-guid-false-positive = "guid_6B29FC40-CA47-1067-B31D-00DD010662DA"
+
+my-guid-true-positive = "guid_yfneB6HiI002LuYfFZnFkis4a3h47Q0dQp4D"
