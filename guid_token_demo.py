1f0efab865928a24299bde2a9c046e8de6db8052
Frederic Malo <frederic.malo@checkmarx.com>
2026-03-06 11:53:02 +0100

    enrtopy

--- guid_token_demo.py
+++ guid_token_demo.py
@@ -1,5 +1,5 @@
 
 
-my-guid-false-positive = "guid_6B29FC40-CA47-1067-B31D-00DD010662DA"
+my-entropy-false-positive = "entropy_6B29FC40-CA47-1067-B31D-00DD010662DA"
 
-my-guid-true-positive = "guid_yfneB6HiI002LuYfFZnFkis4a3h47Q0dQp4D"
+my-entropy-true-positive = "entropy_yfneB6HiI002LuYfFZnFkis4a3h47Q0dQp4D"
